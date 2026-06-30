15509734e58f0cb6597f9a40737f3ac30f0e35e8
Sbillxx <146845774+Sbillxx@users.noreply.github.com>
2026-06-30 16:55:38 +0700

    Fix: resolve git merge conflict in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,4 @@
 <?php
-<<<<<<< HEAD
-// Forward request to the public index.php
-=======
 
 // Paksa Laravel buat simpan file sementara (cache/session/views) di folder /tmp
 // Karena Vercel itu "read-only" (cuma /tmp yang bisa ditulis)
@@ -14,5 +11,4 @@ $_ENV['VIEW_COMPILED_PATH'] = '/tmp';
 $_ENV['CACHE_STORE'] = 'array';
 $_ENV['SESSION_DRIVER'] = 'cookie';
 
->>>>>>> 1590130aa88da3ee0a99c14e1c9ed8371c010d42
 require __DIR__ . '/../public/index.php';
